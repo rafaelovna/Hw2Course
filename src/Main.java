d8a255d1ab5f03f3bc0eda2751a94eedd6229161
rafaelovna <aikanush777@mail.ru>
2022-12-31 03:04:01 +0400

    Сделано.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
 public class Main {
     public static void main(String[] args) {
+        Car lada = new Car(
+                "Lada",
+                "Granta",
+                2015,
+                "Россия",
+                "желтый",
+                1.7);
+        lada.object();
+
+
+        Car audi = new Car(
+                "Audi",
+                "A8 50 L TDI quattro",
+                2020,
+                "Германия",
+                "черный",
+                3.0);
+        audi.object();
+
+
+        Car bmv = new Car(
+                "BMW",
+                "Z8",
+                2021,
+                "Германия",
+                "черный",
+                3.0);
+        bmv.object();
+
+
+        Car kia = new Car(
+                "Kia",
+                "Sportage 4-го поколения",
+                2018,
+                "Южная Корея",
+                "красный",
+                2.4);
+        kia.object();
+
+
+        Car hyundai = new Car(
+                "Hyundai",
+                "Avante",
+                2016,
+                "Южная Корея",
+                "оранжевый",
+                1.6);
+        hyundai.object();
 
     }
 }
